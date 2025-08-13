3661acc90b9a420f963f08565465ab1b007d2541
bahsoonagroprocessingindustries. <37225677+bahsoonagro@users.noreply.github.com>
2025-08-13 13:18:51 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import logo from '/assets/logo.png';
+import logo from '/logo.png';
 
 import Dashboard from './components/Dashboard';
 import RawMaterials from './components/RawMaterials';
