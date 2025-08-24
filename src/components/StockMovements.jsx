78b44a0487a1b76dc126749f9796c0373fac9687
bahsoonagroprocessingindustries. <37225677+bahsoonagro@users.noreply.github.com>
2025-08-24 20:48:24 +0000

    Update StockMovements.jsx

--- src/components/StockMovements.jsx
+++ src/components/StockMovements.jsx
@@ -1,4 +1,4 @@
--import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const API_BASE = "https://backend-repo-ydwt.onrender.com/api/stock-movements";
