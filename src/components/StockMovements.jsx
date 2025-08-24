37aa162fec9d543d816168b581b5c6556fa5d6f0
bahsoonagroprocessingindustries. <37225677+bahsoonagro@users.noreply.github.com>
2025-08-24 20:40:03 +0000

    Update StockMovements.jsx

--- src/components/StockMovements.jsx
+++ src/components/StockMovements.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-const API_BASE = "https://backend-repo-ydwt.onrender.com";
+const API_BASE = "https://backend-repo-ydwt.onrender.com/api/stockmovements";
 
 const StockMovements = () => {
   const [formData, setFormData] = useState({
