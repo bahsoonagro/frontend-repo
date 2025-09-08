e8f73141dc1a20087d591e9f7c5b4c5079ada852
bahsoonagroprocessingindustries. <37225677+bahsoonagro@users.noreply.github.com>
2025-09-08 13:40:02 +0000

    Delete src/components/stock directory

--- src/components/stock/VendorList.jsx
+++
@@ -1,9 +0,0 @@
-// Dashboard.jsx
-import React from 'react';
-
-const Dashboard = () => {
-  return <div className="bg-white shadow p-4 rounded">Dashboard Overview</div>;
-};
-
-export default Dashboard;
-
