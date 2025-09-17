95c3b5d92494f877f41831a9c211a35b291ab6d8
bahsoonagroprocessingindustries. <37225677+bahsoonagro@users.noreply.github.com>
2025-09-17 15:41:01 +0000

    Update api.js

--- src/components/api.js
+++ src/components/api.js
@@ -1,1 +1,10 @@
 
+// Centralized axios instance
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "https://backend-repo-ydwt.onrender.com/api",
+  timeout: 8000,
+});
+
+export default api;
