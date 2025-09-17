3b627b41714ab0fa5d8b8748d4d37b6b30f1af1e
Ahmed Bahsoon <ahmedbahsoon@gmail.com>
2025-09-17 17:59:16 +0000

    Updated dependencies, added electron.js

---
+++ electron.js
@@ -0,0 +1,26 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: false,
+    },
+  });
+
+  // Load React build
+  win.loadFile(path.join(__dirname, "build", "index.html"));
+}
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
+
