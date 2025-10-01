6b75bd4051e4725402063e30db32b2a250f0d3a9
bahsoonagroprocessingindustries. <37225677+bahsoonagro@users.noreply.github.com>
2025-10-01 21:34:11 +0000

    Update FinishedProducts.jsx

--- src/components/FinishedProducts.jsx
+++ src/components/FinishedProducts.jsx
@@ -36,7 +36,7 @@ export default function FinishedProducts() {
     productName: "",
     batchNumber: "",
     productionDate: "",
-    quantityKg: "",
+    quantityctn: "",
     unit: "",
     remarks: "",
   });
